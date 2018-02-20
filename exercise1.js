08afc4e41c6cf3024d3454f45e19bf1a98389e19
Denis <mail@shevchenko.ch>
2018-02-20 23:18:25 +0100

    added exercise 1

---
+++ exercise1.js
@@ -0,0 +1,27 @@
+var alphabetString = 
+		"a=.-;b=-...;c=-.-.;d=-..;e=.;f=..-;g=--.;h=....;i=..;j=.---;k=-.-;l=.-..;"+
+		"m=--;n=-.;o=---;p=.---.;q=--.-;r=.-.;s=...;t=-;u=..-;v=...-;w=.--;x=-..-;"+
+		"y=-.-;z=--..; =//;.=.-.-.-;,=--..--;?=..--..";
+
+function charToMorseCode(char){
+    var res = alphabetString.split(";");
+
+    for(var i = 0; i < res.length; i++){
+        var letterArr = res[i].split("=");
+        if(letterArr[0] === char) return letterArr[1];
+    }
+}
+
+function convertToMorse(str){
+    var charArr = str.split('');
+    var result = "";
+
+    for(var i = 0; i < charArr.length; i++){
+        result += charToMorseCode(charArr[i]);        
+    }
+
+    return result;
+}
+
+console.log(alphabetString.split(";"));
+console.log(convertToMorse("sos"));
